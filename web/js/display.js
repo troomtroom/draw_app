f67901f751737b50546659b267f8115098716df3
troomtroom <pant.tarunpant@gmail.com>
2026-06-21 14:53:09 +0530

    enhance data viewer

--- web/js/display.js
+++ web/js/display.js
@@ -1,4 +1,4 @@
-function createRow(container,userName,samples){
+function createRow(container,userName,samples,user_id){
     const row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
@@ -7,10 +7,15 @@ function createRow(container,userName,samples){
     rowLabel.innerHTML= userName;
     rowLabel.classList.add("rowLabel");
 
+    const rowIdLabel = document.createElement("div");
+    rowIdLabel.innerHTML= user_id;
+    rowIdLabel.classList.add("rowIdLabel");
+
+    rowLabel.appendChild(rowIdLabel);
     row.appendChild(rowLabel);
 
     for(let sample of samples){
-        const {id,label} = sample;
+        const {id,label,user_id} = sample;
 
         // add whitebg to these images
         const sampleContainer = document.createElement("div");
@@ -24,6 +29,9 @@ function createRow(container,userName,samples){
         const img = document.createElement('img');
         img.src = constants.IMG_DIR+'/'+id+'.png';
         img.classList.add("thumb");
+        if(utils.flaggedUsers.includes(user_id)){
+            img.classList.add("blur");
+        }
         sampleContainer.appendChild(img);
         row.appendChild(sampleContainer);
     }
